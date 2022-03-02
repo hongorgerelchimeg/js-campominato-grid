1228ecb238b8827917a3fa91f33b958042915d3e
hongorgerelchimeg <g.hongoroo@gmail.com>
2022-03-02 16:24:15 +0100

    ho dimenticato di pushare.

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+let bombArea = document.querySelector('.campo');
+const inPut = document.querySelector('#level');
+const btnPlay = document.querySelector('#play');
+
+btnPlay.addEventListener('click', function(){
+    level = inPut.value;
+    console.log(level);
+    if (level )
+})
+
+function gridCreator(line) {
+    
+}
